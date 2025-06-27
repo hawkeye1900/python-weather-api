ea44b7f7068dbc21af3c4a92e0d0f53f0f3913fa
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2025-06-27 13:37:07 +0100

    get_weather_data and display_weather re-factored to allow a list of cities to be entered by user

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 
-def get_weather_data(api):
-    city = input("Enter the city name: ")
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric'
+def get_weather_data(api, place):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&appid={
+    api}&units=metric'
 
     try:
         response = requests.get(url)
@@ -32,14 +32,19 @@ def get_weather_data(api):
 
 
 def display_weather():
-    weather = get_weather_data(API_KEY)
+    cities = input("Enter a single city, or enter multiple cities, separated "
+                 "by a comma (e.g Glasgow,Paris,Istanbul): ").split(",")
 
-    if weather:
-        print(f"Weather in {weather['city']}:\n"
-              f"Temperature: {weather['temp']}°C\n"
-              f"Description: {weather['description']}")
-    else:
-        print("Unable to retrieve weather data.")
+    for city in cities:
+        city = city.strip()
+        weather = get_weather_data(API_KEY, city)
+
+        if weather:
+            print(f"Weather in {weather['city']}:\n"
+                  f"Temperature: {weather['temp']}°C\n"
+                  f"Description: {weather['description']}\n")
+        else:
+            print("Unable to retrieve weather data.")
 
 
 if __name__ == "__main__":
